c37a2b582971a2315629ed8ae976f2862638f2c0
Peter Bex <peter@more-magic.net>
2017-07-28 21:32:47 +0200

    Enable initfs-hook and add correct resolution selection to it
    Weston boots now! (No demos yet, though...)

--- aports/device-samsung-g800f/initfs-hook.sh
+++ aports/device-samsung-g800f/initfs-hook.sh
@@ -2,3 +2,4 @@
 
 # set framebuffer resolution
 cat /sys/class/graphics/fb0/modes > /sys/class/graphics/fb0/mode
+echo '720,2560' > /sys/devices/platform/exynos-sysmmu.10/exynos4-fb.0/graphics/fb0/virtual_size
